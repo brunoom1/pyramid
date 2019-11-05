abf042396f368c66c0f8a45a83951957d1d2172d
Gabriel Mendonça <gmendonca@fingerprint.com.br>
2019-11-04 22:44:15 -0300

    Criar renderização e página html

--- pyramid.js
+++ pyramid.js
@@ -2,14 +2,38 @@ class Pyramid {
 
 	constructor (size = 5) {
 		this.size = size;
+		this.setDefault();
+	}
+
+	setDefault () {
+		this.space = " ";
+		this.char = "*";
+		this.newLine = "\n";
+
+		return this;
+	}
+
+	setHtml () {
+		this.space = "<span style='margin-left: 7px; display: inline-block; '></span>";
+		this.char="*";
+		this.newLine="<br />";
+
+		return this;	
+	}
+
+	setSize (size) {
+		this.size = size;
+		return this;
 	}
 
 	toString () {
-		let str_pyramid = "";
-		let total_spaces = this.size - 1;
-		let line_size = total_spaces + this.size;
 
-		for (let i = 1; i <= this.size; i ++) {
+		let size = this.size;
+		let str_pyramid = "";
+		let total_spaces = size - 1;
+		let line_size = total_spaces + size;
+
+		for (let i = 1; i <= size; i ++) {
 
 			const spaces = i - 1;
 			const middle = Math.ceil((line_size - i - spaces) / 2);
@@ -18,29 +42,20 @@ class Pyramid {
 
 			for (let x = 0; x <= line_size + spaces; x ++) {
 				if (x > middle && x < middle + i + spaces ) {
-					str_pyramid += ( c % 2 == 0 ? "*":" ");
+					str_pyramid += ( c % 2 == 0 ? this.char : this.space );
 					c++;
 				}
 				else {
-					str_pyramid += " ";
+					str_pyramid += this.space;
 				}				
 			}
 
-			str_pyramid += "\n";
+			str_pyramid += this.newLine;
 		}
-
 
 		return str_pyramid;		
 	}
 
 }
-
-
-(() => {
-
-	const pyramid = new Pyramid(20);
-	console.log(pyramid.toString());
-
-})()
 
 
